97497423dbc8b096f72cf758377d8246bf5a6c36
박병현 <byeonghyun.park@rihoz.com>
2022-05-06 11:49:55 +0900

    image 와 icon 용량감소, 리소스 정리, 디자인 수정

--- career/js/index.js
+++ career/js/index.js
@@ -1,5 +1,25 @@
 NUM_OF_BUTTONS = 3;
-NUM_OF_QUESTIONS = 9;
+NUM_OF_QUESTIONS = Array.from(document.querySelectorAll(".part .frequent .question")).length;
+
+
+// android 여부 확인
+function checkMobile() {
+
+    var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+
+    if (varUA.indexOf('android') > -1) {
+        //안드로이드
+        return "android";
+    } else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
+        //IOS
+        return "ios";
+    } else {
+        //아이폰, 안드로이드 외
+        return "other";
+    }
+}
+
+
 //아이템이 자연스럽게 등장하는 애니메이션을 주는 함수
 function appearSoft(node) {
     node.style.display = "block";
@@ -19,14 +39,14 @@ function showPartDropdown() {
     document.querySelectorAll(".spec-part").forEach(e => appearSoft(e));
     document.querySelector(".select-part").setAttribute("state", "open");
     document.querySelector(".select-part").style = "border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;"
-    document.querySelector(".select-part img").src = "./Icons/chevron-up.svg";
+    document.querySelector(".select-part img").src = "./icons/ic_chevron-up.svg";
 }
 
 function hidePartDropdown() {
     document.querySelectorAll(".spec-part").forEach(e => { disappearSoft(e, 300) });
     document.querySelector(".select-part").setAttribute("state", "closed")
     document.querySelector(".select-part").style = "border-bottom-left-radius: 5px; border-bottom-right-radius: 5px;"
-    document.querySelector(".select-part img").src = "./Icons/chevron-down.svg";
+    document.querySelector(".select-part img").src = "./icons/ic_chevron-down.svg";
 }
 document.querySelector(".select-part").addEventListener("click", function() {
     if (this.getAttribute("state") == "closed") {
@@ -54,35 +74,54 @@ let answers = document.querySelectorAll('.frequent .answer');
 let questionDropButtons = document.querySelectorAll(`.frequent .down-icon`);
 let questionUpButtons = document.querySelectorAll(`.frequent .up-icon`);
 
+let questionHeight = null;
 
-
+function updateQuestionHeight(index) {
+    questionHeight = Array.from(qnaBox).map((e, i) => {
+        let beforeHeight = 0,
+            afterHeight = 0;
+        let invisible = false;
+        if (e.style.display == "none") {
+            invisible = true;
+        }
+        beforeHeight = e.clientHeight;
+        answers[i].style.display = "block";
+        afterHeight = e.clientHeight;
+        answers[i].style.display = "none";
+        return { beforeHeight, afterHeight };
+    })
+}
 
 function openQuestion(i) {
-    let beforeHeight = 0,
-        afterHeight = 0;
-    beforeHeight = qnaBox[i].clientHeight;
+    // document.querySelectorAll('.part__description>*').forEach((e, index) => {
+    //     if (e.style.display == "block") {
+    //         e.ch
+    //         updateQuestionHeight(index);
+    //     }
+    // })
+    // setTimeout(() => {
+    //         qnaBox[i].style = `height: ${questionHeight[i].afterHeight}px`;
+    //     })
+    // afterHeight = qnaBox[i].clientHeight; //afterhight 의 값을 어느 위치에서 설정하느냐에 따라 answer의 애니메이션이 실행여부가 결정됨...왜그러지..?
     appearSoft(answers[i]);
-    setTimeout(() => {
-        qnaBox[i].style = `height: ${afterHeight}px`;
-    })
-    afterHeight = qnaBox[i].clientHeight; //afterhight 의 값을 어느 위치에서 설정하느냐에 따라 answer의 애니메이션이 실행여부가 결정됨...왜그러지..?
+
     questionUpButtons[i].style = "display: block";
     questionDropButtons[i].style = "display: none";
-    qnaBox[i].style = `background-color: #F8F9FA; height: ${beforeHeight + 1}px;`;
+    qnaBox[i].style = `background-color: #F8F9FA;`;
     qnaBox[i].setAttribute("state", "open");
 }
 
 function closeQuestion(i) {
-    if (i == -1) return;
-    let beforeHeight = 0,
-        afterHeight = 0;
-    beforeHeight = qnaBox[i].clientHeight;
+    if (i == -1 || qnaBox[i].getAttribute("state") == "closed") return;
+    //qnaBox[i].height = `${qnaBox[i].clientHeight}px`;
     answers[i].style = "display: none";
     questionDropButtons[i].style = "display: block";
     questionUpButtons[i].style = "display: none";
     qnaBox[i].setAttribute("state", "closed");
-    afterHeight = qnaBox[i].clientHeight;
     qnaBox[i].style = `background-color: white;`;
+    // setTimeout(() => {
+    //     qnaBox[i].style.height = `${questionHeight.beforeHeight}px`;
+    // })
 
 }
 
@@ -155,6 +194,9 @@ xmlHttp.onreadystatechange = function() {
             appearSoft(document.querySelector(".apply .pop-up-background"))
             appearSoft(document.querySelector(".apply .pop-up.pc"));
         }
+        document.body.style.overflow = "hidden";
+        document.querySelectorAll("body *").forEach(e => e.style.touchAction = "none");
+
     }
 }
 
@@ -215,6 +257,7 @@ window.addEventListener('scroll', () => {
             floatingBtn.style = "opacity: 0";
             floatingBtn.style.cursor = "default";
             document.querySelector(".floating-a").href = "javascript:void(0);";
+
         }
     }
 })
@@ -225,4 +268,8 @@ document.querySelectorAll(".apply .pop-up .out").forEach(out => out.addEventList
     disappearSoft(document.querySelector(".apply .pop-up.pc"), 0.5);
     disappearSoft(document.querySelector(".apply .pop-up.mobile"), 0.5);
     document.querySelectorAll(".apply .sub input").forEach(e => e.value = null);
+    document.body.style.overflow = "visible";
+    document.querySelectorAll("body *").forEach(e => e.style.touchAction = "auto");
+
+    if (checkMobile() == "android" && document.querySelector(".apply .select-part span").innerHTML == "가사청소") window.location.href = 'https://play.google.com/store/apps/details?id=com.rihoz.dangjib.cleaner';
 }))
